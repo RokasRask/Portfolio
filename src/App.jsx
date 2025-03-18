5f1c45db03f47227cfbab6463a39a1a64b69df05
RokRask <rokas.raskevic@gmail.com>
2025-03-18 18:39:59 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Projects from './Components/Projects/Projects';
 import Contact from './Components/Contact/Contact';
 import Footer from './Components/Footer/Footer';
 
+
 // Animate on scroll initialization
 const checkScroll = () => {
   const elements = document.querySelectorAll('.animate-on-scroll');
